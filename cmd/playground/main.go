55accc6bd9a1e2a346024d31be4772ffaa3cbc72
anishpateluk <anish@ninjacode.co.uk>
2021-02-22 19:21:37 +0000

    added hello world playground

--- cmd/playground/main.go
+++ cmd/playground/main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"github.com/joho/godotenv"
 	"fmt"
+	"log"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
 	fmt.Println("hello from playground")
 }
